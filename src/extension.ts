cf88134a6ce02604b5270a0183fa461b76373624
GP <gouthamjp@outlook.com>
2024-12-02 23:45:25 +0530

    base logic complete

--- src/extension.ts
+++ src/extension.ts
@@ -4,31 +4,53 @@ import * as vscode from 'vscode';
 export async function activate(context: vscode.ExtensionContext) {
 
 
-	const fileName = vscode.window.activeTextEditor?.document.fileName;
-	const cssFileName = fileName?.replace(".html",".css");
 
-
-	const content = 'Hello, this is some content written from the VS Code extension!';
-	const data = encoder.encode(content);
-	
-	const uri = vscode.Uri.file(cssFileName!);
-	await vscode.workspace.fs.writeFile(uri, "data");
-	vscode.workspace.onDidChangeTextDocument(event => {
+	vscode.workspace.onDidSaveTextDocument(async (event) => {
         const editor = vscode.window.activeTextEditor;
-        if (editor && event.document === editor.document) {
-            checkForClassAttribute(editor);
+        if (editor && event === editor.document && editor.document.languageId === 'html') {
+            await checkForClassAttribute(editor);
         }
     });
-
-	vscode.workspace.fs.writeFile
-
 }
 
-function checkForClassAttribute(editor? : vscode.TextEditor){
+
+async function checkForClassAttribute(editor? : vscode.TextEditor){
+
 	const text = editor!.document.getText();
 	const classAttributeRegex = /class=["'][^"']*["']/g;
 	const matches = [...text.matchAll(classAttributeRegex)];
-	console.log(matches);
+	const htmlClassNames = matches.map(match => {
+		const classNameMatch = match[0].match(/["']([^"']+)["']/);
+		return classNameMatch ? classNameMatch[1] : '';  
+	});
+
+ 
+
+	const cssFileName = (editor?.document.fileName)?.replace("html","css");
+	const uri = vscode.Uri.file(cssFileName!);
+	await vscode.workspace.fs.stat(uri);
+	const cssFileContent = await vscode.workspace.fs.readFile(uri);
+	const cssText = new TextDecoder().decode(cssFileContent);
+	const cssClassRegex = /\.([a-zA-Z0-9_-]+)/g;
+	const cssMatches = [...cssText.matchAll(cssClassRegex)];
+	const cssClassNames = cssMatches.map(match => match[1]);
+
+	const commonElements = htmlClassNames.filter(item => !cssClassNames.includes(item));
+	await createClass(commonElements, cssFileName!, cssText);
 };
-// This method is called when your extension is deactivated
+
+async function createClass(classNames : string[], cssFileName : string, existingCssContent : string){
+
+	classNames.forEach((className)=> {
+		let temp = `.${className}{} \n \n`
+		existingCssContent = existingCssContent + temp;
+	})
+
+	const encoder = new TextEncoder();
+	const data = encoder.encode(existingCssContent);
+	
+	const uri = vscode.Uri.file(cssFileName!);
+	await vscode.workspace.fs.writeFile(uri, data);
+}
+
 export function deactivate() {}
